2ba8e4b5e07e7ae04917a1f8ffa2f0c861278120
anuj gupta <anujgupta596@gmail.com>
2021-09-25 23:15:35 +0530

    updated

--- Frontend/netflix_clone/src/Components/Header/Header.jsx
+++ Frontend/netflix_clone/src/Components/Header/Header.jsx
@@ -13,9 +13,9 @@ function Header() {
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse " id="navbarSupportedContent">
-      <ul className="navbar-nav w-100 me-auto mb-2 mb-lg-0 ">
+      <ul className="navbar-nav w-100 me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active text-white" aria-current="page" href="/">Home</a>
+          <a className="nav-link active text-white " aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item ">
           <a className="nav-link  text-white" href="/">Movie</a>
@@ -37,7 +37,7 @@ function Header() {
       
       <form className="d-flex me-5">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success  text-white "  type="submit">Search</button>
+
       </form>
 
         <div class="dropdown me-5 ">
@@ -45,7 +45,7 @@ function Header() {
     User
   </a>
 
-  <ul class="dropdown-menu start " aria-labelledby="dropdownMenuLink">
+  <ul class="dropdown-menu dropdown-menu-end " aria-labelledby="dropdownMenuLink">
     <li><a class="dropdown-item" href="/">Switch Profiles</a></li>
     <li><a class="dropdown-item" href="/">Select Plans</a></li>
     <li><a class="dropdown-item" href="/">Purchases</a></li>
