4a2a0efe3fe45fee3917949ab5dac41afe6529fe
Pratik Hopal <70657277+pratikhopal@users.noreply.github.com>
2021-09-25 22:28:51 +0530

    Update serializers.py

--- Backend/netflix/netflix_app/serializers.py
+++ Backend/netflix/netflix_app/serializers.py
@@ -4,14 +4,10 @@ from .models import Video_category, Video_data,App_user_watchlist
 
 
 
-class VideoCategorySerializer(serializers.ModelSerializer):
-    class Meta :
-        model = Video_category
-        fields ="__all__"
 
 class LatestShowSerializer(serializers.ModelSerializer):
     class Meta :
         model = Video_data
-        fields =['title','description']
+        fields ="__all__"
 
 
