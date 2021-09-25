97153641e722065c266178fb3a2e202bb8c43502
Pratik Hopal <70657277+pratikhopal@users.noreply.github.com>
2021-09-25 22:29:06 +0530

    Update urls.py

--- Backend/netflix/netflix_app/urls.py
+++ Backend/netflix/netflix_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import categorylist, latestshowlist,Watchlist
 
 urlpatterns = [
-    path('category/', categorylist),
+    
     path('latest_show/',latestshowlist),
     
 ]
